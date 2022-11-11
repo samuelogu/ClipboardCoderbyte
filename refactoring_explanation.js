2f9d0a1d3a50c6c7dae542129ed9f9a9b7647f72
Samuel Ogu <hello@samuelogu.com>
2022-11-11 17:11:42 +0100

    refactoring function explanation

---
+++ refactoring_explanation.js
@@ -0,0 +1,7 @@
+//REFACTORING EXPLANATION
+
+/*
+
+Part of what I did in the refactoring function is to use ternary operator to make it clean and readable. I also remove the redundant data variable declaration to free up memory allocation
+
+ */
